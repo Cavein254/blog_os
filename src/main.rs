b3682d6757808f41170c562309154712077eb6ad
johnmuthua <caveomuthama@gmail.com>
2023-09-13 20:12:01 +0300

    Added os basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] //disables the std library
+#![no_main] //disable all rust-level entry points
+
+// This function is called on panic
+use core::panic::PanicInfo;
+
+//This function handles panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] //don't change the name of this function
+pub extern "C" fn _start() -> ! {
+    //This is the entry point function
+    loop {}
 }
