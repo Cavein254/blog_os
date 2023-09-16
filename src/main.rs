e6e567ba2346d9c199613614c72dc89f81d1ecd1
johnmuthua <caveomuthama@gmail.com>
2023-09-16 12:58:18 +0300

    Add vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello world sad sad story";
+
 #[no_mangle] //don't change the name of this function
 pub extern "C" fn _start() -> ! {
-    //This is the entry point function
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
